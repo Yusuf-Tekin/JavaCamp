37d7a10ae22a3968f0c1ab69c3b5af4ae225900b
yusuf.tekinx <59294270+Yusuf-Tekin@users.noreply.github.com>
2021-05-02 20:28:40 +0300

    Delete Day4Homework1 directory

--- Day4Homework1/src/Abstracts/CostumerManager.java
+++
@@ -1,8 +0,0 @@
-package Abstracts;
-import Entities.Customer;
-public abstract class CostumerManager implements CustomerService{
-	@Override
-    public void Save(Customer customer) {
-        System.out.println("Saved to db : " + customer.getName());
-    }
-}
--- Day4Homework1/src/Abstracts/CustomerCheckServices.java
+++
@@ -1,9 +0,0 @@
-package Abstracts;
-
-import Entities.Customer;
-
-public interface CustomerCheckServices {
-	
-	 boolean CheckIfRealPerson(Customer customer);
-
-}
