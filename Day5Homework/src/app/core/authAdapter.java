c126029dc8273151d0a71860550a76643ca3e9cd
yusuf.tekinx <tkn.yusuf.40@gmail.com>
2021-05-06 16:18:03 +0300

    Day 5 Homework is commited

---
+++ Day5Homework/src/app/core/authAdapter.java
@@ -0,0 +1,17 @@
+package app.core;
+
+import app.googleAuth.googleAuth;
+
+public class authAdapter implements authService{
+
+	@Override
+	public void authSocialAccount(String email) {
+		// TODO Auto-generated method stub
+		
+		googleAuth auth = new googleAuth();
+		auth.SignUpGoogle(email);
+		
+		
+	}
+	
+}
---
+++ Day5Homework/src/app/googleAuth/googleAuth.java
@@ -0,0 +1,10 @@
+package app.googleAuth;
+
+
+public class googleAuth {
+	
+	public void SignUpGoogle(String email) {
+		System.out.println(email + " google hesabý ile kayýt tamamlandý");
+	}
+
+}
